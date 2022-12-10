3ce20be154ead6486cc320f3e92cbd311c9ed7a3
Laurie Roy <laurieroy.dev@gmail.com>
2022-12-10 10:45:26 -0800

    Add failing tests for turbo

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -12,9 +12,10 @@ class QuotesTest < ApplicationSystemTestCase
 
     click_on "New quote"
 
+    fill_in "Name",	with: "Capybara quote"
+
     assert_selector "h1", text: "New quote"
 
-    fill_in "Name",	with: "Capybara quote"
     click_on "Create quote"
     
     assert_selector "h1", text: "Quotes"
@@ -34,9 +35,12 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
+    # assert_selector "h1", text: "Edit quote"
 
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
+
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
