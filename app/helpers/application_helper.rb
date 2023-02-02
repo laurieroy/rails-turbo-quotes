014e0069f8647a0e0226910c603cc1c99acec877
Laurie Roy <laurieroy.dev@gmail.com>
2023-02-02 09:39:23 -0700

    Refactor form error display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  # refactor to own file (form helper) if grow proj
+  def form_error_notification(object)
+    if object.errors.any?
+      tag.div class: "error-message" do
+        object.errors.full_messages.to_sentence.capitalize
+      end
+    end
+  end
+
   def render_turbo_stream_flash_messages 
     turbo_stream.prepend "flash", partial: "layouts/flash"
   end
