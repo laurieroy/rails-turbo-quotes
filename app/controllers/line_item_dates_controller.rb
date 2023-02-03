437fae12c853fad1e8925853aef0c7d0509f0e40
Laurie Roy <laurieroy.dev@gmail.com>
2023-02-03 14:41:22 -0700

    Implement dates in turbo, oldest on top

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -10,7 +10,10 @@ class LineItemDatesController < ApplicationController
     @line_item_date = @quote.line_item_dates.build(line_item_date_params)
 
     if @line_item_date.save
-      redirect_to quote_path(@quote), notice: "Date was succesfully created."
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Date was succesfully created." }
+        format.turbo_stream { flash.now[:notice] = "Date was succesfully created." }
+      end
     else
       render :new, status: :unprocessable_entity
     end
