ff3c1cc15423ce19b373035888b2324fdd560446
Laurie Roy <laurieroy.dev@gmail.com>
2023-02-08 14:50:44 -0700

    Rename line_item system test; update for total price

--- test/system/line_items_test.rb
+++ test/system/line_items_test.rb
@@ -1,6 +1,6 @@
 require "application_system_test_case"
 
-class LineItemSystemTest < ApplicationSystemTestCase
+class LineItemsSystemTest < ApplicationSystemTestCase
   include ActionView::Helpers::NumberHelper
 
   setup do
@@ -30,6 +30,7 @@ class LineItemSystemTest < ApplicationSystemTestCase
     assert_selector "h1", text: "First quote"
     assert_text "Animation"
     assert_text number_to_currency(1234)
+    assert_text number_to_currency(@quote.total_price)
   end
 
   test "Updating a line item" do
@@ -46,6 +47,7 @@ class LineItemSystemTest < ApplicationSystemTestCase
 
     assert_text "Capybara article"
     assert_text number_to_currency(1234)
+    assert_text number_to_currency(@quote.total_price)
   end
 
   test "Destroy a line item" do
@@ -60,6 +62,6 @@ class LineItemSystemTest < ApplicationSystemTestCase
     within "##{dom_id(@line_item_date)}" do
       assert_no_text @line_item.name
     end
-
+    assert_text number_to_currency(@quote.total_price)
   end
 end
